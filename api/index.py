1d35e0dda09097437c0a54bef98691f6c9c429a7
HARSHAVARDHANRAMGIRI <r.harsha0541@gmail.com>
2025-10-03 12:06:53 +0530

    Simplify api/index.py for Vercel

--- api/index.py
+++ api/index.py
@@ -1,14 +1,8 @@
-"""
-Vercel Serverless Function Entry Point
-This file is required for Vercel to properly deploy Flask applications
-"""
 import sys
 import os
 
-# Add parent directory to path so we can import app
+# Add parent directory to Python path to import app.py
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
+# Import the Flask application
 from app import app
-
-# Vercel serverless function handler
-app = app
